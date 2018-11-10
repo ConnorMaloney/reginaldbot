255d8319ad22a8d3534462ef1142690aa2bec23f
Foopert <foopert@gmail.com>
2018-11-10 11:07:53 -0500

    ADD: Fixed countdown print

--- Logic/goldReg.py
+++ Logic/goldReg.py
@@ -9,25 +9,41 @@ pyautogui.FAILSAFE = True
 print('Crafting gold amulets. Press Ctrl-C to quit.')
 print("Starting bot in...\n")
 for i in range(5,0,-1):
-    sys.stdout.write(str(i)+' ')
-    sys.stdout.flush()
+    print(i, end='')
+    print('\b' * len(str(i)), end='', flush=True)
     time.sleep(1)
+
+#x, y = pyautogui.position()
+ #       positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+  #      print(positionStr, end='')
+   #     print('\b' * len(positionStr), end='', flush=True)
 
 pyautogui.click() # Click brings in focus (have this on osrs window)
 
 try: 
     while True:
-        pyautogui.click(892,48)
-        print("Walking to smelter...\n")
+        # Clicks smelter coordinates
+        pyautogui.click(892,48) 
         for i in range(20,0,-1):
-            sys.stdout.write(str(i)+' ')
-            sys.stdout.flush()
+            #sys.stdout.write(str(i)+' ')
+            #sys.stdout.flush()
+            #time.sleep(1)
+            numStr = "Walking to smelter: " + str(i).rjust(4)
+            print(numStr, end='')
+            print('\b' * len(numStr), end='', flush=True)
             time.sleep(1)
+        print("\n")
+
+        # Clicks bank coordinates
         pyautogui.click(856,186)
-        print("Walking to bank...\n")
         for i in range(20,0,-1):
-            sys.stdout.write(str(i)+' ')
-            sys.stdout.flush()
+            #sys.stdout.write(str(i)+' ')
+            #sys.stdout.flush()
+            #time.sleep(1)
+            numStr = "Walking to bank: " + str(i).rjust(4)
+            print(numStr, end='')
+            print('\b' * len(numStr), end='', flush=True)
             time.sleep(1)
+        print("\n")
 except KeyboardInterrupt:
     print('\nAbort.')
