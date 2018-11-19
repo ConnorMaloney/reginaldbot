5ab1957db751efc73753b09dc87c77184eef8851
Foopert <foopert@gmail.com>
2018-11-18 19:54:00 -0500

    ADD: Setup more camera orientation for lumber reg

--- Logic/lumberReg.py
+++ Logic/lumberReg.py
@@ -1,38 +1,58 @@
 # Import modules
 import pyautogui, time, sys
 
-# Woodcutting bot at Varrock
+# Woodcutting bot at Varrock, near Bank of Gielinor
 
 # Important coordinates
 # COMPASS - Can differ by 8 pixels
 # ~(797, 56) - Position of orientation button 
 # 
 # BANK - Can differ by 2 pixels
-# ~(548, 525) - Position of varrock banker (closest to south door)
+# ~(912, 235) - Position of "Bank bank booth" (closest to south door, from facing oak tree)
 # ~(230, 100) - Position of bank tab 1 (where oak logs are)
 # ~(245, 145) - Position of logs in bank inv
+# ~(600, 68) - Position of close button
 #
-# SMELTER - Can differ by 5 pixels
-# ~(300, 450) - Position of Smelter (Can actually differ by 50 pixels)
-# ~(220, 450) - Position of "Make 1 gold necklace"
-# ~(195, 535) - Position of "Make All gold necklace" after right click (Right click coords causes menu to vary position)
-#
-# MAP COORDS - Can differ by 3 pixels
-# ~(890, 50) - Position of Al Kharid smelter (from first Northward bank teller)
-# ~(850, 185) - Position of Al Kharid bank (from smelter)
+# TREE - Can differ by 2 pixels
+# ~(430, 95) - Position of "Chop down oak" from bank booth, after orientation
+# ~(419, 102)
 # 
-# BUTTON COORDS - Can differ by 10 pixels
+# INVENTORY - Can differ by 10 pixels
 # ~(645, 1015) - Position of inventory button
-# ~(835, 750) - Position of 2nd item in inventoryUniverse-Wallpaper-1920x1080-PIC-WPXH28693
+# ~(915, 970) - Position of last inventory item (should be last oak log)
 #
-#
-# GRAND EXCHANGE COORDS
-# ~(400, 400) - Position of grand exchange clerk (on rightside from right corner)
-
 # Important Notes: Takes ~53 seconds to smelt 23 gold necklaces. Crafting levelup can popup at anytime
 # and interrupt this process. Press spacebar 3-5 times to skip over this.
-
+#
 # Other bots? Progpmaker and Hoontar00
-# Handle emergency termination
-pyautogui.PAUSE = 2
+#
+# 1.3 seconds after each command, DO NOT CHANGE, WILL AFFECT CAMERA ORIENTATION
+pyautogui.PAUSE = 1.3
+# Handle emergency termination (move mouse to upper left of screen)
 pyautogui.FAILSAFE = True
+
+print('Cutting oak logs. Press Ctrl-C to quit.')
+print("Starting reginald in...\n")
+for i in range(5,0,-1):
+    print(i, end='')
+    print('\b' * len(str(i)), end='', flush=True)
+    time.sleep(1)
+
+# Orients camera north
+pyautogui.click(797, 56)
+#pyautogui.scroll(-100) # Ensure that game is scrolled out as far as possible
+pyautogui.keyDown('up')
+pyautogui.keyUp('up')
+
+try:
+    pyautogui.keyDown('right')
+    pyautogui.keyUp('right')
+ #   for i in range(3,0,-1): 
+        # Testing
+  #      pyautogui.keyDown('right')
+   #     pyautogui.keyUp('right')
+    #    pyautogui.keyDown('left')
+     #   pyautogui.keyUp('left')
+
+except KeyboardInterrupt:
+    print('\nAbort.')
