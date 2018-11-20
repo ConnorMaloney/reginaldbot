7a92b4d794df1194139210ca23a344b91a397b5d
Foopert <foopert@gmail.com>
2018-11-20 00:50:30 -0500

    ADD: FIxed accuracy

--- DesktopReg/lumberRegDesk.py
+++ DesktopReg/lumberRegDesk.py
@@ -43,14 +43,14 @@ try:
             print('\b' * len(numStr), end='', flush=True)
             time.sleep(1)
         print("\n")
-        pyautogui.moveTo(956, 661) # Hover over chop
-        pyautogui.click(956, 661) # Click chop
-        for i in range(0,20,+1):
+        pyautogui.moveTo(956, 635) # Hover over chop
+        pyautogui.click(956, 635) # Click chop
+        for i in range(0,30,+1):
             pyautogui.click(956, 602) #Clicking on stump (doesnt move)
             numStr = "Chopping" + str(i).rjust(4) + " times..."
             print(numStr, end='')
             print('\b' * len(numStr), end='', flush=True)
-            time.sleep(5)
+            time.sleep(3)
         print("\n")
 
         pyautogui.moveTo(1408, 234) # Hover over bank booth button
